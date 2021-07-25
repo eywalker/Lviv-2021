3e8eaad57926dbd7db01565116491a969615ca63
Edgar Y. Walker <edgar.walker@gmail.com>
2021-07-25 10:14:10 -0500

    Update setup.py and add proper dependency

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-  
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 from os import path
@@ -6,15 +5,11 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 setup(
-    name="iviv",
+    name="lviv",
     version="0.1",
-    description="",
-    author="Zhuokun Ding, Edgar Walker",
-    author_email="zhuokund@bcm.edu",
+    description="A collection of code for LVIV 2021 workshop on Deep Learning in Neuroscience",
+    author="Edgar Y. Walker, Zhuokun Ding",
+    author_email="edgar.walker@uni-tuebingen.edu",
     packages=find_packages(exclude=[]),
-    install_requires=[
-        "neuralpredictors",
-        "torch",
-        "numpy"
-    ],
+    install_requires=["neuralpredictors~=0.0.1", "torch", "numpy"],
 )
